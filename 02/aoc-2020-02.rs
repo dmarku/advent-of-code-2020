a4184d14628eb48ff1af750b2aeb6839694a3b7f
Markus Dittmann <dmarku@posteo.de>
2020-12-02 22:03:18 +0100

    day 02: implement policy defined in part II

--- 02/aoc-2020-02.rs
+++ 02/aoc-2020-02.rs
@@ -20,12 +20,24 @@ fn main() {
     }
 
     let mut password_count_v1: usize = 0;
+    let mut password_count_v2: usize = 0;
+
     for line in s.lines() {
         match parse_line(&line) {
             Some(Entry {min, max, letter, password}) => {
+                // check policy defined in Part I
                 let count = count_letter(&password, &letter);
                 if count >= min && count <= max {
                     password_count_v1 += 1;
+                }
+
+                // check policy defined in Part II
+                let char_at_min = password.chars().nth(min - 1);
+                let char_at_max = password.chars().nth(max - 1);
+                if let (Some(a), Some(b)) = (char_at_min, char_at_max) {
+                    if (a == letter && b != letter) || (a != letter && b == letter) {
+                        password_count_v2 += 1;
+                    }
                 }
             },
             None => (),
@@ -33,6 +45,7 @@ fn main() {
     }
 
     println!("{} passwords are valid according to part 1", password_count_v1);
+    println!("{} passwords are valid according to part 2", password_count_v2);
 }
 
 struct Entry<'a> {

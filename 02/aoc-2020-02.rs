06cb0213329cc3685ab664f261237bed98c8e24f
Markus Dittmann <dmarku@posteo.de>
2020-12-02 21:30:13 +0100

    reduce complexity with ? operator and .split_at()
    .split_at() requires nightly at the moment (not yet in Rust 1.48)

--- 02/aoc-2020-02.rs
+++ 02/aoc-2020-02.rs
@@ -19,26 +19,40 @@ fn main() {
         Ok(_) => ()
     }
 
-    let mut password_count: usize = 0;
+    let mut password_count_v1: usize = 0;
     for line in s.lines() {
-        if let Some((policy, password)) = line.split_once(": ") {
-            if let Some((range, letter)) = policy.split_once(" ") {
-                if let Some((min, max)) = range.split_once("-") {
-                    let _min = min.parse::<usize>().unwrap();
-                    let _max = max.parse::<usize>().unwrap();
-                    if let Some(_letter) = letter.chars().nth(0) {
-                        let count = count_letter(&password, &_letter);
-                        if count >= _min && count <= _max {
-                            password_count += 1;
-                        }
-                    }
+        match parse_line(&line) {
+            Some(Entry {min, max, letter, password}) => {
+                let count = count_letter(&password, &letter);
+                if count >= min && count <= max {
+                    password_count_v1 += 1;
                 }
-            }
+            },
+            None => (),
         }
     }
 
-    println!("{} passwords are valid", password_count);
+    println!("{} passwords are valid according to part 1", password_count_v1);
 }
+
+struct Entry<'a> {
+    min: usize,
+    max: usize,
+    letter: char,
+    password: &'a str,
+}
+
+fn parse_line(line: &str) -> Option<Entry> {
+    let (policy, password) = line.split_once(": ")?;
+    let (range, letter) = policy.split_once(" ")?;
+    let (min, max) = range.split_once("-")?;
+    let _min = min.parse::<usize>().ok()?;
+    let _max = max.parse::<usize>().ok()?;
+    let _letter = letter.chars().nth(0)?;
+
+    Some(Entry { min: _min, max: _max, letter: _letter, password })
+}
+
 
 fn count_letter(string: &str, letter: &char) -> usize {
     string.chars().filter(|c| c == letter).count()

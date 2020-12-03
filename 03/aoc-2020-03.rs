0ce69bb70601f9d590503d66d4d6d020d7194c93
Markus Dittmann <dmarku@posteo.de>
2020-12-03 21:06:31 +0100

    day 03: improve function order, variable names

--- 03/aoc-2020-03.rs
+++ 03/aoc-2020-03.rs
@@ -8,33 +8,6 @@ enum Tile {
     Unknown,
 }
 
-fn main() {
-    let path = Path::new("input.txt");
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
-
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => (),
-    }
-
-    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-
-    let mut product = 1;
-    for (inc_x, inc_y) in slopes {
-        let tree_count = check_slope(&s, inc_x, inc_y);
-        println!("Right {}, down {}: {} trees", inc_x, inc_y, tree_count);
-        product *= tree_count;
-    }
-
-    println!("tree count product: {}", product);
-}
-
 fn parse_character(c: &char) -> Tile {
     match c {
         '.' => Tile::Open,
@@ -43,13 +16,13 @@ fn parse_character(c: &char) -> Tile {
     }
 }
 
-fn check_slope(s: &str, increment_x: usize, increment_y: usize) -> usize {
+fn check_slope(input: &str, increment_x: usize, increment_y: usize) -> usize {
     // current position
     let mut x = 0;
     let mut y = 0;
 
     let mut tree_count = 0;
-    for line in s.lines() {
+    for line in input.lines() {
         if (y % increment_y) == 0 {
             if let Some(Tile::Tree) = line
                 .chars()
@@ -64,4 +37,31 @@ fn check_slope(s: &str, increment_x: usize, increment_y: usize) -> usize {
     }
 
     tree_count
+}
+
+fn main() {
+    let path = Path::new("input.txt");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut input = String::new();
+    match file.read_to_string(&mut input) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => (),
+    }
+
+    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+    let mut product = 1;
+    for (inc_x, inc_y) in slopes {
+        let tree_count = check_slope(&input, inc_x, inc_y);
+        println!("Right {}, down {}: {} trees", inc_x, inc_y, tree_count);
+        product *= tree_count;
+    }
+
+    println!("tree count product: {}", product);
 }

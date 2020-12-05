208f78775f9395a40efa154f832a64814e3b836a
Markus Dittmann <dmarku@posteo.de>
2020-12-05 18:44:40 +0100

    day 05: solve part 2

--- 05/aoc-2020-05.rs
+++ 05/aoc-2020-05.rs
@@ -1,5 +1,7 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
+use std::iter::FromIterator;
 use std::path::Path;
 
 fn id(seat: &str) -> u16 {
@@ -15,12 +17,8 @@ fn id(seat: &str) -> u16 {
     })
 }
 
-fn row(id: u16) -> u16 {
+fn row(id: &u16) -> u16 {
     id >> 3
-}
-
-fn column(id: u16) -> u16 {
-    id & 7
 }
 
 fn main() {
@@ -40,7 +38,22 @@ fn main() {
 
     let ids = input.lines().map(|ref seat| id(seat));
 
-    if let Some(highest_id) = ids.max() {
+    if let Some(highest_id) = ids.clone().max() {
         println!("highest seat ID is {}", highest_id);
+    }
+
+    let id_set = HashSet::<u16>::from_iter(ids);
+    let unlisted_ids = (0..2u16.pow(10))
+        // constraint 1: seat isn't in the list of booked IDs
+        .filter(|ref n| !(id_set.contains(*n)))
+        // constraint 2: seat isn't in the first or last row
+        .filter(|ref n| (row(n) != 0 && row(n) != 127))
+        // constraint 3: seats with ID + 1 and ID - 1 are booked
+        .filter(|ref n| id_set.contains(&(*n + 1)) && id_set.contains(&(*n - 1)));
+
+    println!("unlisted ids:");
+
+    for id in unlisted_ids {
+        println!("{}", id)
     }
 }

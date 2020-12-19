424b501b77abe7204b11c8e289c8bea3357a2627
Markus Dittmann <dmarku@posteo.de>
2020-12-19 21:25:51 +0100

    day 11: incorrect solution for part 2

--- 11/src/main.rs
+++ 11/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
@@ -63,7 +64,7 @@ impl State {
             .unwrap_or(&Tile::Floor)
     }
 
-    fn neighbors(&self, row_index: usize, seat_index: usize) -> Vec<&Tile> {
+    fn get_neighbors(&self, row_index: usize, seat_index: usize) -> Vec<&Tile> {
         let mut v = Vec::with_capacity(8);
         if row_index >= 1 {
             if seat_index >= 1 {
@@ -85,6 +86,139 @@ impl State {
 
         v
     }
+
+    fn occupied_count(&self, row_index: usize, seat_index: usize) -> usize {
+        let mut occupied_seats = 0;
+
+        //north
+        {
+            let steps = row_index;
+            let mut occupied = false;
+            for step in 1..steps {
+                if let Tile::OccupiedSeat = self.get_seat(row_index - step, seat_index) {
+                    occupied = true;
+                }
+            }
+            if occupied {
+                occupied_seats += 1;
+            }
+        }
+
+        //northeast
+        {
+            let steps = cmp::min(row_index, self.layout[0].len() - seat_index);
+            let mut occupied = false;
+            for step in 1..steps {
+                if let Tile::OccupiedSeat = self.get_seat(row_index - step, seat_index + step) {
+                    occupied = true;
+                }
+            }
+            if occupied {
+                occupied_seats += 1;
+            }
+        }
+        //east
+        {
+            let steps = self.layout[0].len() - seat_index;
+            let mut occupied = false;
+            for step in 1..steps {
+                if let Tile::OccupiedSeat = self.get_seat(row_index, seat_index + step) {
+                    occupied = true;
+                }
+            }
+            if occupied {
+                occupied_seats += 1;
+            }
+        }
+
+        //southeast
+        {
+            let steps = cmp::min(
+                self.layout.len() - row_index,
+                self.layout[0].len() - seat_index,
+            );
+            let mut occupied = false;
+            for step in 1..steps {
+                if let Tile::OccupiedSeat = self.get_seat(row_index + step, seat_index + step) {
+                    occupied = true;
+                }
+            }
+            if occupied {
+                occupied_seats += 1;
+            }
+        }
+
+        //south
+        {
+            let steps = self.layout.len() - row_index;
+            let mut occupied = false;
+            for step in 1..steps {
+                if let Tile::OccupiedSeat = self.get_seat(row_index + step, seat_index) {
+                    occupied = true;
+                }
+            }
+            if occupied {
+                occupied_seats += 1;
+            }
+        }
+
+        //southwest
+        {
+            let steps = cmp::min(self.layout.len() - row_index, seat_index);
+            let mut occupied = false;
+            for step in 1..steps {
+                if let Tile::OccupiedSeat = self.get_seat(row_index + step, seat_index - step) {
+                    occupied = true;
+                }
+            }
+            if occupied {
+                occupied_seats += 1;
+            }
+        }
+
+        //west
+        {
+            let steps = seat_index;
+            let mut occupied = false;
+            for step in 1..steps {
+                if let Tile::OccupiedSeat = self.get_seat(row_index, seat_index - step) {
+                    occupied = true;
+                }
+            }
+            if occupied {
+                occupied_seats += 1;
+            }
+        }
+
+        //northwest
+        {
+            let steps = cmp::min(row_index, seat_index);
+            let mut occupied = false;
+            for step in 1..steps {
+                if let Tile::OccupiedSeat = self.get_seat(row_index - step, seat_index - step) {
+                    occupied = true;
+                }
+            }
+            if occupied {
+                occupied_seats += 1;
+            }
+        }
+
+        occupied_seats
+    }
+}
+
+impl fmt::Debug for State {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in &self.layout {
+            for seat in row {
+                f.write_fmt(format_args!("{:?}", &seat))?;
+            }
+            f.write_str("\n")?;
+        }
+
+        fmt::Result::Ok(())
+    }
 }
 
 fn step(state: &State) -> State {
@@ -92,7 +226,7 @@ fn step(state: &State) -> State {
     for (row_index, row) in state.layout.iter().enumerate() {
         let mut seats = Vec::new();
         for (seat_index, seat) in row.iter().enumerate() {
-            let neighbors = state.neighbors(row_index, seat_index);
+            let neighbors = state.get_neighbors(row_index, seat_index);
 
             let next = match seat {
                 Tile::EmptySeat => {
@@ -130,10 +264,43 @@ fn step(state: &State) -> State {
     State { layout: rows }
 }
 
+fn step_pt2(state: &State) -> State {
+    let mut rows = Vec::new();
+    for (row_index, row) in state.layout.iter().enumerate() {
+        let mut seats = Vec::new();
+        for (seat_index, seat) in row.iter().enumerate() {
+            let occupied_seats = state.occupied_count(row_index, seat_index);
+
+            let next = match seat {
+                Tile::EmptySeat => {
+                    if occupied_seats == 0 {
+                        Tile::OccupiedSeat
+                    } else {
+                        *seat
+                    }
+                }
+                Tile::OccupiedSeat => {
+                    if occupied_seats >= 5 {
+                        Tile::EmptySeat
+                    } else {
+                        Tile::OccupiedSeat
+                    }
+                }
+                other => *other,
+            };
+
+            seats.push(next);
+        }
+        rows.push(seats);
+    }
+
+    State { layout: rows }
+}
+
 fn main() {
-    let input = read_input("input.txt");
-    //let input = read_input("input_example_1.txt");
-    let mut state = State {
+    //let input = read_input("input.txt");
+    let input = read_input("input_example_1.txt");
+    let initial_state = State {
         layout: input
             .lines()
             .map(|l| {
@@ -149,26 +316,54 @@ fn main() {
             .collect(),
     };
 
-    let mut next_state = step(&state);
-    let mut steps = 1;
-    while next_state != state {
-        state = next_state;
-        next_state = step(&state);
-        steps += 1;
+    println!("--- part I --------------------");
+
+    {
+        let mut state = State {
+            layout: initial_state.layout.clone(),
+        };
+        let mut next = step(&state);
+        let mut steps = 1;
+
+        while state != next {
+            steps += 1;
+            state = next;
+            next = step(&state);
+        }
+
+        println!("{} steps until equlibrium", steps);
+        let occupied_seats: usize = state
+            .layout
+            .iter()
+            .map(|row| row.iter().filter(|&s| *s == Tile::OccupiedSeat).count())
+            .sum();
+        println!("{} seats occupied", occupied_seats);
     }
 
-    println!("{:?}", next_state.layout);
-    println!("{} steps until equlibrium", steps);
-    let occupied_seats: usize = next_state
-        .layout
-        .iter()
-        .map(|row| row.iter().filter(|&s| *s == Tile::OccupiedSeat).count())
-        .sum();
-    println!("{} seats occupied", occupied_seats);
+    println!("--- part II --------------------");
+    {
+        let mut state = State {
+            layout: initial_state.layout.clone(),
+        };
+        let mut next = step_pt2(&state);
+        let mut steps = 1;
 
-    println!("part I");
-    println!("TODO");
+        while state != next {
+            steps += 1;
+            state = next;
+            next = step_pt2(&state);
+        }
 
-    println!("part II");
-    println!("TODO");
+        println!("{} steps until equlibrium", steps);
+        let occupied_seats: usize = state
+            .layout
+            .iter()
+            .map(|row| row.iter().filter(|&s| *s == Tile::OccupiedSeat).count())
+            .sum();
+        println!("{} seats occupied", occupied_seats);
+    }
+
+    let first_step = step_pt2(&initial_state);
+    let second_step = step_pt2(&first_step);
+    println!("{:?}", second_step);
 }

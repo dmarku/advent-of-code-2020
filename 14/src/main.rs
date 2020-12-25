f8b5f92bfba0e4ae122782d9a599cfd42ccb7a9e
Markus Dittmann <dmarku@posteo.de>
2020-12-25 02:54:39 +0100

    day 14: split parsing & execution

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::prelude::*;
 use std::path::Path;
 use std::{collections::HashMap, fs::File};
+use std::{num::ParseIntError, str::FromStr};
+use thiserror::Error;
 
 fn read_input(filename: &str) -> String {
     let path = Path::new(filename);
@@ -38,35 +40,75 @@ fn main() {
         memory: HashMap::new(),
     };
 
+    enum Instruction {
+        Mask(String),
+        Mem { address: usize, value: u64 },
+    }
+
+    #[derive(Error, Debug)]
+    enum InstructionError {
+        #[error("wrong 'mask' instruction format")]
+        WrongMask,
+        #[error("wrong 'mem' instruction format")]
+        WrongMem,
+        #[error("unknown instruction")]
+        UnknownInstruction,
+        #[error(transparent)]
+        ParseError(#[from] ParseIntError),
+    }
+
+    impl FromStr for Instruction {
+        type Err = InstructionError;
+
+        fn from_str(line: &str) -> Result<Instruction, InstructionError> {
+            if line.starts_with("mask = ") {
+                let bits = line
+                    .strip_prefix("mask = ")
+                    .ok_or(InstructionError::WrongMask)?;
+                return Ok(Instruction::Mask(bits.to_owned()));
+            } else if line.starts_with("mem[") {
+                if let (Some(start), Some(end)) = (line.find('['), line.find(']')) {
+                    let address: &usize = &line[start + 1..end].parse()?;
+                    let value_offset = 3 + line.find(" = ").ok_or(InstructionError::WrongMem)?;
+                    let value: u64 = (&line[value_offset..]).parse()?;
+
+                    return Ok(Instruction::Mem {
+                        address: *address,
+                        value,
+                    });
+                } else {
+                    return Err(InstructionError::WrongMem);
+                }
+            } else {
+                return Err(InstructionError::UnknownInstruction);
+            }
+        }
+    }
+
     for line in input.lines() {
-        if line.starts_with("mask = ") {
-            let bits = line.strip_prefix("mask = ").unwrap();
-            state.mask_1 = bits.chars().fold(0, |n, c| {
-                (n << 1)
-                    | match c {
-                        '1' => 1,
-                        _ => 0,
-                    }
-            });
-            state.mask_0 = bits.chars().fold(!0, |n, c| {
-                (n << 1)
-                    | match c {
-                        '0' => 0,
-                        _ => 1,
-                    }
-            });
-        } else if line.starts_with("mem[") {
-            if let (Some(start), Some(end)) = (line.find('['), line.find(']')) {
-                let address: &usize = &line[start + 1..end].parse().unwrap();
-                let value: u64 = line
-                    .find(" = ")
-                    .and_then(|i| Some(&line[i + 3..]))
-                    .and_then(|s| s.parse().ok())
-                    .unwrap();
+        match line.parse::<Instruction>() {
+            Ok(Instruction::Mask(bits)) => {
+                state.mask_1 = bits.chars().fold(0, |n, c| {
+                    (n << 1)
+                        | match c {
+                            '1' => 1,
+                            _ => 0,
+                        }
+                });
+                state.mask_0 = bits.chars().fold(!0, |n, c| {
+                    (n << 1)
+                        | match c {
+                            '0' => 0,
+                            _ => 1,
+                        }
+                });
+            }
+            Ok(Instruction::Mem { address, value }) => {
                 state
                     .memory
-                    .insert(*address, value & state.mask_0 | state.mask_1);
+                    .insert(address, value & state.mask_0 | state.mask_1);
             }
+            _ => (),
         }
     }
 

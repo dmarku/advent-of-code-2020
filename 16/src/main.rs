8d22be417b620421ecaae3a7648f646f5be5c400
Markus Dittmann <dmarku@posteo.de>
2020-12-27 23:40:03 +0100

    day 16: solve part 2

--- 16/src/main.rs
+++ 16/src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
 use std::io::prelude::*;
+use std::ops::RangeInclusive;
 use std::path::Path;
-use std::{fs::File, ops::RangeInclusive};
 
 fn read_input(filename: &str) -> String {
     let path = Path::new(filename);
@@ -28,7 +30,6 @@ struct Field {
 
 fn main() {
     let input = read_input("input.txt");
-    println!("{}", input);
 
     // sections are separated by empty lines
     let mut sections = input.split("\n\n");
@@ -55,39 +56,115 @@ fn main() {
         Some(Field { name, ranges })
     }
 
-    let fields = fields_section
-        .lines()
-        .filter_map(parse_field)
-        .collect::<Vec<Field>>();
+    let fields: Vec<Field> = fields_section.lines().filter_map(parse_field).collect();
 
-    for f in &fields {
-        println!("{:?}", f);
-    }
-
-    let tickets: Vec<_> = nearby_tickets_section
+    let tickets: Vec<Vec<usize>> = nearby_tickets_section
         .lines()
-        .map(|l| {
-            l.split(',')
-                .filter_map(|s| s.parse::<usize>().ok())
-                .collect::<Vec<_>>()
-        })
+        // skip section header()
+        .skip(1)
+        .map(|l| l.split(',').filter_map(|s| s.parse().ok()).collect())
         .collect();
 
     println!("--- part I ------------------------------------------");
 
+    fn value_is_valid(fields: &[Field], value: &usize) -> bool {
+        fields
+            .iter()
+            .flat_map(|f| &f.ranges)
+            .any(|r| r.contains(value))
+    }
+
     let error_rate: usize = tickets
         .iter()
         .flatten()
-        .filter(|&value| {
-            !fields
-                .iter()
-                .flat_map(|f| &f.ranges)
-                .any(|r| r.contains(value))
-        })
+        .filter(|&value| !value_is_valid(&fields, value))
         .sum();
 
     println!("the error rate is {:?}", error_rate);
 
     println!("--- part II -----------------------------------------");
-    println!("TODO");
+
+    let my_ticket: Vec<usize> = my_ticket_section
+        .lines()
+        .nth(1)
+        .unwrap()
+        .split(',')
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    fn ticket_is_valid(fields: &[Field], ticket: &[usize]) -> bool {
+        ticket.iter().all(|value| value_is_valid(fields, value))
+    }
+
+    let mut valid_tickets: Vec<&Vec<usize>> = tickets
+        .iter()
+        .filter(|t| ticket_is_valid(&fields, t))
+        .collect();
+
+    valid_tickets.push(&my_ticket);
+
+    // collect values for each position in the tickets
+    let mut values_by_field_index: Vec<HashSet<&usize>> = Vec::with_capacity(my_ticket.len());
+    for i in 0..my_ticket.len() {
+        let values = valid_tickets.iter().map(|t| &t[i]);
+        values_by_field_index.push(values.collect());
+    }
+
+    let mut indices_by_field: HashMap<&str, HashSet<usize>> = fields
+        .iter()
+        .map(|f| {
+            (
+                &f.name[..],
+                (0..values_by_field_index.len()).collect::<HashSet<_>>(),
+            )
+        })
+        .collect();
+
+    let fields_by_name: HashMap<&str, &Field> = fields.iter().map(|f| (&f.name[..], f)).collect();
+
+    // first round - filter indices whose values are outside the field's valid ranges
+    for (&name, indices) in indices_by_field.iter_mut() {
+        let matching_indices = indices.iter().filter_map(|i| {
+            if values_by_field_index.get(*i).unwrap().iter().all(|v| {
+                fields_by_name
+                    .get(name)
+                    .unwrap()
+                    .ranges
+                    .iter()
+                    .any(|r| r.contains(v))
+            }) {
+                Some(*i)
+            } else {
+                None
+            }
+        });
+
+        *indices = matching_indices.collect();
+    }
+
+    /*
+    for entry in &indices_by_field {
+        println!("{:?}", entry);
+    }
+    */
+
+    let mut field_index_map = HashMap::<&str, usize>::new();
+
+    while let Some((name, indices)) = indices_by_field.iter().find(|(_, v)| v.len() == 1) {
+        let i = *indices.iter().nth(0).unwrap();
+        field_index_map.insert(&name, i);
+        for values in indices_by_field.values_mut() {
+            values.remove(&i);
+        }
+    }
+
+    //println!("{:?}", field_index_map);
+
+    let p: usize = field_index_map
+        .iter()
+        .filter(|(name, _)| name.starts_with("departure"))
+        .map(|(_, index)| my_ticket[*index])
+        .product();
+
+    println!("product of my ticket's departure field values is {}", p);
 }

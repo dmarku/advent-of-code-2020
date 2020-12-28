b0e85dc60a94deba54fa95a29caa01df5ba047cc
Markus Dittmann <dmarku@posteo.de>
2020-12-28 10:38:43 +0100

    day 17: add blank slate

---
+++ 17/src/main.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn read_input(filename: &str) -> String {
+    let path = Path::new(filename);
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut input = String::new();
+    match file.read_to_string(&mut input) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => (),
+    }
+
+    input
+}
+
+fn main() {
+    let input = read_input("input.txt");
+    println!("{}", input);
+
+    println!("--- part I ------------------------------------------");
+    println!("TODO");
+
+    println!("--- part II -----------------------------------------");
+    println!("TODO");
+}
